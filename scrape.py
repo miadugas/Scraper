855cf309b27ee2f62f1c22ef3782e5982407ac10
Mia Morticia <miamorticia@Mias-MacBook-Pro.local>
2020-12-04 17:49:53 -0700

    base build

---
+++ scrape.py
@@ -0,0 +1,12 @@
+import requests
+from bs4 import BeautifulSoup
+
+url = "https://finance.yahoo.com/quote/GOOGL?p=GOOGL&.tsrc=fin-srch"
+
+# putting this in to make it appear that a browser is making the query's and not a bot :p
+headers = {'User-Agent':'Mozilla/5.0 (Macintosh; Intel Mac OS X 11_0_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.67 Safari/537.36' }
+
+html_page = requests.get(url)
+
+print(html_page.content)
+
